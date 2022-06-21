f4a7ae1ec00e7d0ba6e9e45f5ecb635d6968a268
Saurabh250 <65411015+Saurabh250@users.noreply.github.com>
2022-06-22 02:56:51 +0530

    22 June

--- power_fly-main/lib/login.dart
+++ power_fly-main/lib/login.dart
@@ -1,4 +1,6 @@
-// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors
+// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, sort_child_properties_last, unnecessary_import
+
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
@@ -6,19 +8,60 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      color: Colors.black,
         child: Column(
       children: [
-        Image.asset("assests/images/authentication_1.png"),
-        Text("Welcome Back!",
-        style: TextStyle(fontSize: 22),
+        Padding(padding: EdgeInsets.fromLTRB(20, 70, 20, 0)),
+        Image.asset(
+          "assets/images/authentication_1.png",
+          height: 200,
+          width: 200,
         ),
-
-        Text("Please, Log In.",
-        style: TextStyle(fontSize: 30, 
-        fontWeight: FontWeight.bold),
+        SizedBox(
+          height: 10,
         ),
-      
+        Text(
+          "Welcome Back!",
+          style: TextStyle(fontSize: 20),
+        ),
+        Text(
+          "Please, Log In.",
+          style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 60.0),
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(labelText: "Enter your username"),
+              ),
+              TextFormField(
+                obscureText: true,
+                decoration: InputDecoration(labelText: "Enter your password"),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text("Log In"),
+                style: ElevatedButton.styleFrom(
+                 primary: Color.fromRGBO(227, 252, 191, 100),
+                  side: BorderSide(width: 3,color: Colors.brown),
+                  elevation: 3,
+                  textStyle: const TextStyle(fontSize: 40),
+                  fixedSize: const Size(300, 50),
+                  padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                  shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                  
+                ),),
+              ),
+            ],
+          ),
+        ),
       ],
     ));
   }
--- power_fly-main/lib/main.dart
+++ power_fly-main/lib/main.dart
@@ -1,16 +1,20 @@
-// ignore_for_file: use_key_in_widget_constructors, unused_import
+// ignore_for_file: use_key_in_widget_constructors, unused_import, use_full_hex_values_for_flutter_colors
 
 import 'package:flutter/material.dart';
 import 'package:power_fly/login.dart';
 
+void main() {
+  runApp(MyApp());
+  
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      themeMode: ThemeMode.light,
+      themeMode: ThemeMode.dark,
       theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
+        scaffoldBackgroundColor: const Color(212227),
       ),
       routes: {
         "/": (context) => Login(),
